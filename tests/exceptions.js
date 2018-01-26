44146d4ddb1f25bb506f817e04c48d96fd414c21
jrivera <jrivera@brightcove.com>
2018-01-26 17:15:01 -0500

    Use  instead of the rejection side of  to make the promise chain easier to read and fix tests so that they don't incorrectly pass when no exception is thrown

--- tests/exceptions.js
+++ tests/exceptions.js
@@ -2,40 +2,54 @@ const expect = require('chai').expect;
 const {dispatch, ramdap: R} = require('../');
 const errors = require('../lib/errors')['en-us'];
 
-describe('parameter exception handling', function () {
-  const obj = {};
-  const noop = () => {};
+const obj = {};
+const noop = () => {};
+const retFn = () => noop;
+const thrower = () => { throw new Error('oops!'); };
+const retFnThrower = () => thrower;
+const testThrower = () => {
+  throw new Error('A rejection was expected but the test case resulted in a successfully resolved promise');
+};
 
+describe('parameter exception handling', function () {
   it('getDispatchFn is not a function', function () {
     const testCase = dispatch(noop, 'not a function');
 
-    return testCase(obj).catch((err) => {
-      expect(err.message).to.equal(errors.INVALID_FN_PARAMS(['getDispatchFn']));
-    });
+    return testCase(obj)
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.message).to.equal(errors.INVALID_FN_PARAMS(['getDispatchFn']));
+      });
   });
 
   it('predicateFn is not a function', function () {
     const testCase = dispatch('not a function', noop);
 
-    return testCase(obj).catch((err) => {
-      expect(err.message).to.equal(errors.INVALID_FN_PARAMS(['predicateFn']));
-    });
+    return testCase(obj)
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.message).to.equal(errors.INVALID_FN_PARAMS(['predicateFn']));
+      });
   });
 
   it('valueLens is not a function', function () {
     const testCase = dispatch(noop, noop, 'not a function');
 
-    return testCase(obj).catch((err) => {
-      expect(err.message).to.equal(errors.INVALID_FN_PARAMS(['valueLens']));
-    });
+    return testCase(obj)
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.message).to.equal(errors.INVALID_FN_PARAMS(['valueLens']));
+      });
   });
 
   it('predicateFn is not a function', function () {
     const testCase = dispatch(noop, noop);
 
-    return testCase().catch((err) => {
-      expect(err.message).to.equal(errors.VALUE_NIL());
-    });
+    return testCase()
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.message).to.equal(errors.VALUE_NIL());
+      });
   });
 });
 
@@ -53,52 +67,56 @@ describe('result of function exception handling', function () {
 });
 
 describe('function thrown exception handling', function () {
-  const obj = {};
-  const noop = () => {};
-  const retFn = () => noop;
-  const thrower = () => { throw new Error('oops!'); };
-  const retFnThrower = () => thrower;
-
   it('getDispatchFn throws', function () {
     const testCase = dispatch(noop, thrower);
 
-    return testCase(obj).catch((err) => {
-      expect(err.dispatchMessage).to.equal(errors.DISPATCH_FN(undefined));
-    });
+    return testCase(obj)
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.dispatchMessage).to.equal(errors.DISPATCH_FN(undefined));
+      });
   });
 
   it('predicateFn throws', function () {
     const testCase = dispatch(thrower, noop);
 
-    return testCase(obj).catch((err) => {
-      expect(err.dispatchMessage).to.equal(errors.PREDICATE_FN(obj));
-    });
+    return testCase(obj)
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.dispatchMessage).to.equal(errors.PREDICATE_FN(obj));
+      });
   });
 
   it('dispatcher throws', function () {
     const testCase = dispatch(noop, retFnThrower);
 
-    return testCase(obj).catch((err) => {
-      expect(err.dispatchMessage).to.equal(errors.DISPATCHER_FN());
-    });
+    return testCase(obj)
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.dispatchMessage).to.equal(errors.DISPATCHER_FN());
+      });
   });
 
   it('lens getter throws', function () {
     const lensThrower = R.lens(thrower, R.identity);
     const testCase = dispatch(noop, retFn, lensThrower);
 
-    return testCase(obj).catch((err) => {
-      expect(err.dispatchMessage).to.equal(errors.GET_LENS());
-    });
+    return testCase(obj)
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.dispatchMessage).to.equal(errors.GET_LENS());
+      });
   });
 
   it('lens setter throws', function () {
     const lensThrower = R.lens( R.identity, thrower);
     const testCase = dispatch(noop, retFn, lensThrower);
 
-    return testCase(obj).catch((err) => {
-      expect(err.dispatchMessage).to.equal(errors.SET_LENS());
-    });
+    return testCase(obj)
+      .then(testThrower)
+      .catch((err) => {
+        expect(err.dispatchMessage).to.equal(errors.SET_LENS());
+      });
   });
 
 });
