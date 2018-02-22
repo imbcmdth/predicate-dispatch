30a490029bd48f6013223c686afc7bdafc98cc9b
jrivera <jrivera@brightcove.com>
2018-02-22 17:02:14 -0500

    Added a few extra functions to the “ramdap” utility

--- utils/ramdap.js
+++ utils/ramdap.js
@@ -6,13 +6,19 @@ const B = require('bluebird');
 // Like Ramda.prop except it accepts promises OR values for every argument and _always_ returns a promise
 const propP = curry((prop, obj) => B.all([prop, obj]).then(([prop, obj]) => B.resolve(obj[prop])));
 
+// Like Ramda.path except it accepts promises OR values for every argument and _always_ returns a promise
+const pathP = curry((pathArr, obj) => B.all([pathArr, obj]).then(([pathArr, obj]) => B.reduce(pathArr, (subObj, pathSpec) => propP(pathSpec, subObj), obj)));
+
 // Like Ramda.props except it accepts promises OR values for every argument and _always_ returns a promise
 const propsP = curry((props, obj) => B.all([props, obj]).then(([props, obj]) => B.all(props.map(propP(__, obj)))));
 
 // Like Ramda.assoc except it accepts promises OR values for every argument and _always_ returns a promise
 const assocP = curry((prop, val, obj) => B.all([prop, val, obj]).then(([prop, val, obj]) => assoc(prop, val, obj)));
 
-// Like Ramda.assoc except it sets the `prop` property to the return value of a function
+// Like Ramda.assoc except it sets the `prop` property to the return value of `fn`
+const assocFn = curry((prop, fn, obj) => assoc(prop, fn(obj), obj));
+
+// Like Ramda.assoc except it sets the `prop` property to the return value of  `fn`
 // and accepts promises OR values for every argument and _always_ returns a promise
 const assocFnP = curry((prop, fn, obj) => assocP(prop, fn(obj), obj));
 
@@ -20,6 +26,8 @@ const assocFnP = curry((prop, fn, obj) => assocP(prop, fn(obj), obj));
 // This is necessary because Ramda.composeP is a little dumb if
 // a function in the pipeline does not return a promise
 const asPromise = (fn) => compose(B.resolve, fn);
+
+const get = curry((prop, map) => map.get(prop));
 
 const getP = curry((prop, map) => B.all([prop, map]).then(([prop, map]) => B.resolve(map.get(prop))));
 
@@ -45,11 +53,14 @@ const propSatisfiesP = curry((fn, prop, obj) => B.all([fn, propP(prop, obj)]).th
 module.exports = merge(require('ramda'), {
   propP,
   propsP,
+  pathP,
+  get,
   getP,
   assocP,
   lensP,
   overP,
   propSatisfiesP,
+  assocFn,
   assocFnP,
   asPromise
 });
